4a066313aa9880e00a337c03e211681d2846da09
Sojin <118108221+kimSojinn@users.noreply.github.com>
2024-05-23 14:46:07 +0900

    프로젝트 초기 세팅
    react + tailwindcss + typescript

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const px0_1600 = { ...Array.from(Array(1601)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,tsx}"],
+  mode: "jit",
+  theme: {
+    screens: {
+      mobile: { max: "768px" },
+      tablet: { max: "1400px" },
+      desktop: "2560px",
+    },
+    spacing: px0_1600,
+    borderWidth: px0_200,
+    borderRadius: px0_200,
+    fontSize: px0_200,
+    lineHeight: px0_200,
+    minWidth: px0_1600,
+    minHeight: px0_1600,
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
